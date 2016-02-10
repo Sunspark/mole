3ae59c71891d6d1c993af35acc7de42ceac20a4a
wlist <williamealist@gmail.com>
2016-02-10 16:56:29 +0000

    beginning attempt at a business model

--- src/main/java/mole/model/business/BusinessComment.java
+++ src/main/java/mole/model/business/BusinessComment.java
@@ -1,4 +1,18 @@
 package mole.model.business;
 
+import lombok.Data;
+import mole.model.dao.Comment;
+import mole.model.repositories.CommentRepository;
+import mole.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BusinessComment {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
 }
