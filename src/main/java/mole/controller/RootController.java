36c62a22a04cb03e1783c1119217bc8dcbbd0870
wlist <williamealist@gmail.com>
2016-03-09 15:51:51 +0000

    showing custom property access

--- src/main/java/mole/controller/RootController.java
+++ src/main/java/mole/controller/RootController.java
@@ -1,11 +1,16 @@
 package mole.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RootController {
+    @Value("${custom.property}")
+    String customTest;
+
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String hello() {
-        return "hello";
+        return "hello " + customTest;
     }
 }
